53c35cda316b18c4e71bbe5f3655ecc20700282b
diogosfpinto <diogo.sfpinto@gmail.com>
2020-11-30 19:33:33 -0400

    added module parser for receiving data in json formate

--- src/config/custom-express.js
+++ src/config/custom-express.js
@@ -5,6 +5,14 @@ require('marko/express');
 //Recebe como retorno uma função
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+
+//Setando um middleware para o express
+//Criado para receber dados do formulário adição de livros
+//Permite o envio de dados no estilo Json
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 
 module.exports = app;
 
