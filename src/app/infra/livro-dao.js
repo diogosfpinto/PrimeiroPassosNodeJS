b0b2964905b92b3ec04a7372d107bb7975c0b962
diogosfpinto <diogo.sfpinto@gmail.com>
2020-12-01 21:31:10 -0400

    added methods update, delete and updateById

--- src/app/infra/livro-dao.js
+++ src/app/infra/livro-dao.js
@@ -4,6 +4,55 @@ class LivroDao {
         this._db = db;
     }
 
+    buscarPorId(id) {
+        return new Promise((resolve, reject) => {
+            this._db.get(
+                'SELECT * FROM livros WHERE id = ?',
+                [id],
+                function(erro, resultado){
+                    if (erro) return reject('Não foi possível buscar o livro desejado!');
+
+                    return resolve(resultado);
+                }
+            )
+        });
+    }
+
+    atualiza(livro) {
+        return new Promise((resolve, reject) => {
+            this._db.run(`
+                UPDATE livros SET 
+                titulo = ?,
+                preco = ?,
+                descricao = ?
+                WHERE id = ?
+            `,
+            [
+                livro.titulo,
+                livro.preco,
+                livro.descricao,
+                livro.id
+            ],
+            function(erro) {
+                if (erro) return reject('Não foi possível atualizar o livro selecionado.');
+
+                return resolve();
+            });
+        });
+    }
+
+    remove(id) {
+        return new Promise((resolve, reject) => {
+            this._db.run("DELETE FROM livros WHERE id = ?",
+            [id],
+            function(erro){
+                if (erro) return reject('Não foi possível remover o livro selecionado');
+
+                return resolve();
+            })
+        })
+    }
+
     adiciona(livro) {
         return new Promise((resolve, reject) => {
             this._db.run(`
