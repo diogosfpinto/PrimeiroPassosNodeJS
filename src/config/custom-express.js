8c9799009bf5eda6626f12f687623fb302e5b0bc
diogosfpinto <diogo.sfpinto@gmail.com>
2020-12-02 21:34:38 -0400

    refactoring way for validate if is an update or insert to manipulate the datebase

--- src/config/custom-express.js
+++ src/config/custom-express.js
@@ -6,6 +6,7 @@ require('marko/express');
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const methodOverride = require('method-override');
 
 //Requisições para um objeto javascript estático será redirecionado para o caminho abaixo
 app.use('/estatico', express.static('src/app/public'));
@@ -16,6 +17,18 @@ app.use('/estatico', express.static('src/app/public'));
 app.use(bodyParser.urlencoded({
     extended: true
 }));
+
+//Usando middleware para permitir a substituição do método de envio do form
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(methodOverride(function (req, res) {
+  if (req.body && typeof req.body === 'object' && '_method' in req.body) {
+    // look in urlencoded POST bodies and delete it
+    var method = req.body._method;
+    delete req.body._method;
+    return method;
+  }
+}));
+
 
 //Adicionando middleware para contagem de tempo para executar uma rota
 app.use('*', function(req, res, next){
