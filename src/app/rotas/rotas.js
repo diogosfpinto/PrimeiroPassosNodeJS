71f4228e45f9dc805fba7bad02d9933008abaa40
diogosfpinto <diogo.sfpinto@gmail.com>
2020-12-03 22:04:08 -0400

    acessing html page without marko file

--- src/app/rotas/rotas.js
+++ src/app/rotas/rotas.js
@@ -1,23 +1,13 @@
 const LivroDao = require('../infra/livro-dao');
 const db = require('../../config/database');
 
+const path = require('path');
+
 //exportando uma função que recebe um parâmetro.
 module.exports = (app) => {
     app.get('/', function(req, resp){
-        resp.send(
-            `<!DOCTYPE html>
-                <html lang="pt-br">
-                    <head>
-                        <title>Casa do Código</title>
-                        <meta charset="UTF-8">
-                        <meta name="viewport" content="width=device-width"> 
-                    </head>
-                
-                    <body>    
-                        <h1>Olá mundo!</h1>
-                    </body>
-                </html>`
-        );
+        //Renderizando página HTML sem a nessidade de um arquivo Marko.
+        resp.sendFile(path.join(__dirname+'/ ../index.html'));
     });
     
     app.get('/livros', function(req, resp){
